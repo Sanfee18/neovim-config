902518c38e568926756ea7e782d5d05f32b0e860
David Sanfelix <d.sanfelix2001@gmail.com>
2024-10-30 19:16:50 +0100

    Added markdown and copilot plugins

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -67,7 +67,7 @@ return {
 					local lspconfig = require("lspconfig")
 					lspconfig.pyright.setup {
 						capabilities = capabilities,
-					}
+				}
 				end
 			}
 		})
@@ -86,25 +86,28 @@ return {
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif require('luasnip').expand_or_jumpable() then
-						require('luasnip').expand_or_jump()
-					else
-						fallback()
-					end
-				end, {"i", "s"}),
-
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif require('luasnip').jumpable(-1) then
-						require('luasnip').jump(-1)
-					else
-						fallback()
-					end
-				end, {"i", "s"}),
+				--
+				-- !!!! Disabled because of conflicts with `copilot` autocomplete !!!!
+				--
+				-- ["<Tab>"] = cmp.mapping(function(fallback)
+				-- 	if cmp.visible() then
+				-- 		cmp.select_next_item()
+				-- 	elseif require('luasnip').expand_or_jumpable() then
+				-- 		require('luasnip').expand_or_jump()
+				-- 	else
+				-- 		fallback()
+				-- 	end
+				-- end, {"i", "s"}),
+				--
+				-- ["<S-Tab>"] = cmp.mapping(function(fallback)
+				-- 	if cmp.visible() then
+				-- 		cmp.select_prev_item()
+				-- 	elseif require('luasnip').jumpable(-1) then
+				-- 		require('luasnip').jump(-1)
+				-- 	else
+				-- 		fallback()
+				-- 	end
+				-- end, {"i", "s"}),
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "dockerfile" },
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "dockerfile", "markdown", "markdown_inline" },
 			sync_install = false,
 			highlight = { enable = true },
 			-- indent = { enable = true },
